83e99965b4a70e7607be2cc052fe52f9222b865a
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-06 00:13:30 -0500

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,11 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+    let rev_str = ""
+    for(let i = str.length-1; i>=0;i--){
+        rev_str+=str[i]
+    }
+    return rev_str
+}
 module.exports = reverse;
