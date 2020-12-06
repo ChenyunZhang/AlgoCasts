1e94797d58340ea6a1aaccf014026309b4a6674f
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-06 14:13:31 -0500

    find maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let map = new Map()
+    let final=0
+    let result
+    for(let char of str){
+        if(map.has(char)){
+            map.set(char, (map.get(char))+1)
+        }else{
+            map.set(char, 1)
+        }
+    }
+    for(let [key,val] of map){
+        if(final<val){
+            final = val
+            result = key
+        }
+    }
+    return result
+}
 
 module.exports = maxChar;
