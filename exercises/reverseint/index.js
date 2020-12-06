ac40f098d9a4af5cfbef9299b281c9fffa0294ae
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-06 12:33:07 -0500

    reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// note pseudocode
+// if the number is negative, make it a postive number
+// check if int%10 = 0, if yes, return the number, 
+// if not, int/10
+
+function reverseInt(n) {
+    let reverse = 0
+    let positive_n = n > 0 ? n : -n
+    while( Math.floor(positive_n) > 0){
+        let temp= positive_n%10
+        positive_n = Math.floor(positive_n/10)
+        reverse = reverse * 10 + temp
+    }
+    return n>=0 ? reverse : reverse*-1
+}
+
+console.log(reverseInt(0))
 
 module.exports = reverseInt;
